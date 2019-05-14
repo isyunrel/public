3f0f1a49b86e4dcd9891f85317467936bc9b8088
isyunrel <47309783+isyunrel@users.noreply.github.com>
2019-05-14 10:49:53 +0800

    add

--- docs/js/words.js
+++ docs/js/words.js
@@ -1,5 +1,33 @@
-﻿(function(root, factory) {
-    root.words = new factory
-})(window, function() {
+﻿((root, factory) => { root.words = new factory(); })(window, function() {
+
+this.words = [
+
+
+
+'theory|理论',
+'analysis|分析',
+'utilization|利用',
+'evaluation|评测',
+'purpose|目的',
+'Synthesis|综合',
+'prictice|实践',
+'application|应用',
+'Instructional|教学',
+'comprehension|理解',
+'resource|资源',
+'material|材料',
+'intenger|整数',
+'integration|整合',
+'demonstration|示范'
+
+
+
+];
+
+this.wordSplit = (str) => {
+    return str.split('|');
+}
 
 });
+
+console.log(words.words);
